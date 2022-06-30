50a15be99a578bb28a265cf6fc9d3c666c1106f6
traceybit <traceymangin@gmail.com>
2022-06-30 11:28:53 -0700

    udpate bsai and goa buffers based on harvest specifications.

--- code/create_database.R
+++ code/create_database.R
@@ -19,7 +19,7 @@ db_gsheet <- "https://docs.google.com/spreadsheets/d/1F_7i9cX2ComJtWUdXo8CoSR9Uj
 db_df     <- read_sheet(db_gsheet)
 buffer_df <- read_sheet(db_gsheet, sheet = 2, col_types = "ccccccdddddccc")
 pgf_df    <- read_sheet(db_gsheet, sheet = 3)
-gao_gf_df <- read_sheet(db_gsheet, sheet = 4, col_types = "ccdddddc")
+goa_gf_df <- read_sheet(db_gsheet, sheet = 4, col_types = "ccdddddc")
 bsai_gf_df <- read_sheet(db_gsheet, sheet = 5, col_types = "ccdddddc")
 
 ## main database
@@ -35,7 +35,7 @@ db_df2 <- db_df %>%
 buffer_df2 <- buffer_df %>%
   ## create id
   mutate(id = paste(council_short, FMP_FEP, stock, common_name, sci_name, sep = '-')) %>%
-  select(id, p_star:act_buffer, buffer_notes = notes, buffer_ref = ref)
+  select(id, p_star:update_schedule, buffer_notes = notes, buffer_ref = ref)
 
 ## pacific groundfish
 pgf_df2 <- pgf_df %>%
@@ -59,6 +59,8 @@ pgf_df3 <- pgf_df2 %>%
          abc_buffer = ABC_BUFFER_FRACTION) %>%
   mutate(acl_buffer = NA,
          act_buffer = NA,
+         year = 2023,
+         update_schedule = "annual",
          buffer_notes = NA,
          buffer_ref = "GMT008 - Draft Annual Groundfish Harvest Specifications")
 
@@ -69,9 +71,121 @@ buffer_df_adj <- buffer_df2 %>%
   select(-pgf) %>%
   rbind(pgf_df3)
 
+## revise goa groundfish
+## ---------------------------------
+
+goa_gf_b <- buffer_df %>%
+  mutate(id = paste(council_short, FMP_FEP, stock, common_name, sci_name, sep = '-')) %>%
+  mutate(goa_gf = str_extract(id, 'NPFMC-GOA groundfish FMP'),
+         goa_gf = ifelse(is.na(goa_gf), 'other', goa_gf)) %>%
+  filter(goa_gf != 'other') %>%
+  mutate(year = 2023) %>%
+  select(id, stock, common_name, p_star, year, update_schedule, ref)
+
+goa_hsp <- goa_gf_df %>%
+  rename(stock = Species) %>%
+  select(-Catch, - ref) %>%
+  filter(!is.na(OFL),
+         Area %in% c('GOA-wide', 'Total', 'AK Total')) %>%
+  mutate(abc_buffer = 1 - (ABC / OFL),
+         acl_buffer = 0,
+         act_buffer = 1 - (TAC / ABC)) %>%
+## recpde to join with original buffer table
+  mutate(stock = str_to_sentence(stock)) %>%
+  mutate(stock=recode(stock,
+                     "Shallow-water flatfish" = "Shallow water flatfish",
+                     "Deep-water flatfish" = "Deepwater flatfish",
+                     "Rougheye and blackspotted rockfish" = "Rougheye & blackspotted rockfish",
+                     "Octopuses" = "Octopus")) %>%
+  filter(Year == 2023) %>%
+  rename(year = Year) %>%
+  select(buff_match = stock, year, abc_buffer, acl_buffer, act_buffer)
+
+## update skates so that buffer match is on common name, update other rockfish for one match
+## udpate nothern and southern rock sole to be shallow water flatfish
+goa_gf_b_adj <- goa_gf_b %>%
+  mutate(buff_match = ifelse(common_name == "Big skate", common_name,
+                             ifelse(common_name == "longnose skates", "Longnose skate",
+                                    ifelse(common_name == "other skates", "Other skates",
+                                           ifelse(stock %in% c("Other rockfish-slope sub-group", "Other rockfish-demersal sub-group"), "Other rockfish",
+                                                  ifelse(stock %in% c("Northern rock sole", "Southern rock sole"), "Shallow water flatfish", stock)))))) %>%
+  left_join(goa_hsp) %>%
+  mutate(buffer_notes = NA) %>%
+  rename(buffer_ref = ref)
+
+## join to buffer table
+buffer_df_adj2 <- buffer_df_adj %>%
+  mutate(goagf = str_extract(id, "NPFMC-GOA groundfish FMP"),
+         goagf = ifelse(is.na(goagf), "other", goagf)) %>%
+  filter(goagf != "NPFMC-GOA groundfish FMP") %>%
+  select(-goagf) %>%
+  rbind(goa_gf_b_adj %>% select(id, p_star, abc_buffer, acl_buffer, act_buffer, year, update_schedule, buffer_notes, buffer_ref))
+
+
+## repeat for bsai
+## ---------------------------------------
+
+## area extact
+bsai_gf_b <- buffer_df %>%
+  mutate(id = paste(council_short, FMP_FEP, stock, common_name, sci_name, sep = '-')) %>%
+  mutate(bsai_gf = str_extract(id, 'NPFMC-Groundfish of the Bering Sea and Aleutian Islands FMP'),
+         bsaigf = ifelse(is.na(bsai_gf), 'other', bsai_gf)) %>%
+  filter(bsai_gf != 'other') %>%
+  mutate(year = 2023) %>%
+  select(id, stock, common_name, p_star, year, update_schedule, ref)
+
+## harvest specifications
+bsai_hsp <- bsai_gf_df %>%
+  rename(stock = Species) %>%
+  select(-`Catch as of11062021`, -ref) %>%
+  filter(!is.na(OFL),
+         stock != "Total") %>%
+  ## recode to join with original buffer table
+  mutate(stock = str_to_sentence(stock),
+         stock = ifelse(stock %in% c("Pollock", "Pacific cod"), paste(stock, Area, sep = "-"), stock)) %>%
+  mutate(stock=recode(stock,
+                      "Pollock-EBS" = "Eastern Bering Sea pollock",
+                      "Pollock-AI" = "Aleutian Islands pollock",
+                      "Pollock-Bogoslof" = "Bogoslof Island Pollock",
+                      "Pacific cod-AI" = "Aleutian Islands Pacific cod",
+                      "Pacific cod-BS" = "Eastern Bering Sea Pacific Cod",
+                      "Greenland turbot" = "Greenland Turbot",
+                      "Northern rock sole" = "Northern Rock sole",
+                      "Blackspotted/rougheye rockfish" = "Rougheye & blackspotted rockfish",
+                      "Octopuses" = "Octopus")) %>%
+  mutate(abc_buffer = 1 - (ABC / OFL),
+         acl_buffer = 0,
+         act_buffer = 1 - (TAC / ABC)) %>%
+  filter(year == 2023) %>%
+  select(stock, year, abc_buffer, acl_buffer, act_buffer)
+
+## join harvest sp. values to df
+bsai_gf_b_adj <- bsai_gf_b %>%
+  left_join(bsai_hsp) %>%
+  mutate(buffer_notes = NA) %>%
+  rename(buffer_ref = ref)
+
+## join to buffer table
+buffer_df_adj3 <- buffer_df_adj2 %>%
+  mutate(bsaigf = str_extract(id, "NPFMC-Groundfish of the Bering Sea and Aleutian Islands FMP"),
+         bsaigf = ifelse(is.na(bsaigf), "other", bsaigf)) %>%
+  filter(bsaigf != "NPFMC-Groundfish of the Bering Sea and Aleutian Islands FMP") %>%
+  select(-bsaigf) %>%
+  rbind(bsai_gf_b_adj %>% select(id, p_star, abc_buffer, acl_buffer, act_buffer, year, update_schedule, buffer_notes, buffer_ref))
+
+
+
+
+
+
+
 ## join buffers with other information
+## ---------------------------------------
+
 full_db <- db_df2 %>%
-  left_join(buffer_df_adj)
+  left_join(buffer_df_adj3)
+
+
 
 
 # Format data
