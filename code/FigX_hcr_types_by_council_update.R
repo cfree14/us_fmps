f681b298edc7848287fd1d21edc7a1ca6f825abf
traceybit <traceymangin@gmail.com>
2022-08-08 10:51:23 -0700

    udpate fig 4

--- code/FigX_hcr_types_by_council_update.R
+++ code/FigX_hcr_types_by_council_update.R
@@ -11,10 +11,10 @@ library(tidyverse)
 
 # Directories
 plotdir <- "figures"
-outputdir <- "data/stock_hcr"
+outputdir <- "data/data_base"
 
 # Read data
-data_orig <- read_csv(file.path(outputdir, "fmp_hcr.csv"))
+data_orig <- read_csv(file.path(outputdir, "hcr_stock_db.csv"))
 
 # figure theme
 ## ----------------------------------------------------------------------------
@@ -34,74 +34,10 @@ my_theme <-  theme(axis.text=element_text(size=7),
 
 
 
-# Format data
+# Format data for figure
 ################################################################################
 
-fmps_n <- data_orig %>%
-  # filter out Fish Resource of the Arctic FMP
-  filter(FMP_FEP != 'Fish Resource of the Arctic FMP') %>%
-  # FMP vs FEP
-  mutate(plan_type = str_extract(FMP_FEP, "FMP"),
-         plan_type = ifelse(is.na(plan_type), "FEP", plan_type))
-
-plan_breakdown <- fmps_n %>%
-  select(FMP_FEP, plan_type) %>%
-  unique() %>%
-  group_by(plan_type) %>%
-  summarise(n = n()) %>%
-  ungroup()
-
-stocks_n <- fmps_n %>%
-  # select unique council-stock combo
-  select(council_short, FMP_FEP, stock, type) %>%
-  unique() %>%
-  mutate(id = paste(council_short, FMP_FEP, stock, sep = "-")) %>%
-  group_by(id) %>%
-  mutate(n = n()) %>%
-  ungroup()
-
-# make changes to correct duplicates
-data_adj <- data_orig %>%
-  left_join(stocks_n) %>%
-  mutate(adj_type = ifelse(n == 2 & council_short %in% c("CFMC") & type == "catch prohibited", "constant catch",
-                           ifelse(n == 2 & council_short %in% c("NOAA") & type == "catch prohibited", "constant F", type)),
-         adj_stock = ifelse(id == "NPFMC-GOA groundfish FMP-Deepwater flatfish" & common_name == "Dover sole", paste(stock, common_name, sep = "-"),
-                            ifelse(id == "NPFMC-GOA groundfish FMP-Deepwater flatfish" & common_name != "Dover sole", paste0(stock, "- except Dover sole"),
-                                   ifelse(id == "NPFMC-GOA groundfish FMP-Demersal shelf rockfish" & common_name == "yelloweye rockfish", paste(stock, common_name, sep = "-"),
-                                          ifelse(id == "NPFMC-GOA groundfish FMP-Demersal shelf rockfish" & common_name != "yelloweye rockfish", paste(stock, "- except yelloweye rockfish"),
-                                                 ifelse(id == "NPFMC-Groundfish of the Bering Sea and Aleutian Islands FMP-Skates", common_name,
-                                                        ifelse(id == "GMFMC-Gulf of Mexico Reef Fish FMP-Shallow water grouper" & common_name == "Red grouper", paste(stock, common_name, sep = "-"),
-                                                               ifelse(id == "GMFMC-Gulf of Mexico Reef Fish FMP-Shallow water grouper" & common_name != "Red grouper", paste(stock, "- except Red grouper"), stock))))))))
-
-
-stocks_n_adj <- data_adj %>%
-  # filter out Fish Resource of the Arctic FMP
-  filter(FMP_FEP != 'Fish Resource of the Arctic FMP') %>%
-  # select unique council-stock combo
-  select(council_short, FMP_FEP, adj_stock, adj_type) %>%
-  unique() %>%
-  mutate(id = paste(council_short, FMP_FEP, adj_stock, sep = "-")) %>%
-  group_by(id) %>%
-  mutate(n = n()) %>%
-  ungroup()
-
-
-# Build data
-data <- data_orig %>%
-  # filter out Fish Resource of the Arctic FMP
-  filter(FMP_FEP != 'Fish Resource of the Arctic FMP') %>%
-  # Recode HCR type
-  mutate(type=ifelse(is.na(type), "Unknown", type),
-         type=stringr::str_to_sentence(type),
-         type=ifelse(grepl("Ramped", type), "Threshold F", type),
-         type=recode(type,
-                     # "None"="Unknown",
-                     "Constant f"="Constant F",
-                     "Downward sloping"="Exempt",
-                     "International exception"="Exempt",
-                     "Stepped f"="Threshold F")) %>%
-  # change to Catch prohibited
-  mutate(type = ifelse(council_short == "SAFMC" & stock == "Nassau Grouper", "Catch prohibited", type)) %>%
+hcr_type_council <- data_orig %>%
   # Order HCR types
   mutate(type=factor(type, levels=c("Threshold F", "Constant F", "Constant escapement",
                                     "Constant catch", "Catch-based", "Catch prohibited", "None", "Exempt", "Unknown") %>% rev())) %>%
@@ -118,14 +54,14 @@ data <- data_orig %>%
   rename(council=council_short)
 
 ## create version of all councils combined
-data_all <- data %>%
+data_all <- hcr_type_council %>%
   group_by(type) %>%
   summarise(n = sum(n)) %>%
   ungroup() %>%
   mutate(prop = n / sum(n)) %>%
   mutate(council = "All councils") %>%
   select(council, type, n, prop) %>%
-  rbind(data)  %>%
+  rbind(hcr_type_council)  %>%
   mutate(council=factor(council,
                         levels=c("All councils", "NEFMC", "MAFMC", "SAFMC", "GMFMC", "CFMC", "PFMC", "NPFMC", "WPFMC",
                                  "NOAA", "NEFMC/MAFMC", "GMFMC/SAFMC", "PFMC/WPFMC") %>% rev())) %>%
@@ -134,8 +70,8 @@ data_all <- data %>%
   ungroup() %>%
   mutate(council_n = paste0(council, " (n = ", total_n, ")")) %>%
   mutate(council_n=factor(council_n,
-                          levels=c("All councils (n = 504)", "NEFMC (n = 30)", "MAFMC (n = 13)", "SAFMC (n = 37)", "GMFMC (n = 23)",
-                                   "CFMC (n = 52)", "PFMC (n = 228)", "NPFMC (n = 64)", "WPFMC (n = 9)",
+                          levels=c("All councils (n = 507)", "NEFMC (n = 30)", "MAFMC (n = 13)", "SAFMC (n = 37)", "GMFMC (n = 23)",
+                                   "CFMC (n = 52)", "PFMC (n = 228)", "NPFMC (n = 67)", "WPFMC (n = 9)",
                                    "NOAA (n = 23)", "NEFMC/MAFMC (n = 3)", "GMFMC/SAFMC (n = 6)", "PFMC/WPFMC (n = 16)") %>% rev()))
 
 # Plot data
